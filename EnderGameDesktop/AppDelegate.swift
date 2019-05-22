f1f3b17685f973b47641351a7f68df6b32457416
Miquel Mirat Soler <1166miki@gmail.com>
2019-05-22 20:04:30 +0200

    Initial Commit

---
+++ EnderGameDesktop/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  EnderGameDesktop
+//
+//  Created by Miquel Mirat Soler on 22/05/2019.
+//  Copyright © 2019 mmirat. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
